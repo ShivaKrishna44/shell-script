1f761baec2b6be3052250faf2ad7adcffd09a5cf
ShivaKrishna <shivakrishna44@gmail.com>
2024-12-30 16:25:23 +0530

    02-small-program.sh

---
+++ 02-small-program.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# used for comments
+PRINT="Hello world"
+echo $PRINT
+
+#!/bin/bash
+#Creates a new variable with a value of "Hello World"
+learningbash="Hello World"
+echo $learningbash
